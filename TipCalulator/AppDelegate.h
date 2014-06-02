8facf87e7da1ccd698bf462385da8814ba85eb4a
Manish Gujjar <manish_gujjar@yahoo.co.in>
2014-06-02 12:52:16 -0700

    TipCalculator
    TipCalculator

---
+++ TipCalulator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalulator
+//
+//  Created by Manish Gujjar on 5/30/14.
+//  Copyright (c) 2014 Manish Gujjar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalulator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalulator
+//
+//  Created by Manish Gujjar on 5/30/14.
+//  Copyright (c) 2014 Manish Gujjar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
